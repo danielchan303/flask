15b183ebe9e847c24e0dc886c6a2ee4400d9eab8
Chan Daniel <danielchan303@gmail.com>
2018-09-04 21:28:48 +0800

    Form added

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,13 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def hello_world():
-    return render_template("layout.html")
+    method_post = request.method == 'POST'
 
+
+    return render_template("layout.html", name=request['name'])
